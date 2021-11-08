383e8ed466b7945b0e91b55348dbb9ceb9c8bc24
reyna <reynaldo.martinez1030@gmail.com>
2021-11-07 20:18:00 -0500

    javacode

--- src/main/java/baseline/CreateItem.java
+++ src/main/java/baseline/CreateItem.java
@@ -11,27 +11,46 @@ public class CreateItem{
 
     public Item startProcess(String desc, LocalDate date){
         //create an instance of Item
+        Item newItem = new Item();
         //create instance of InvalidData
+        PopUp popup = new PopUp();
         //call validateDescription and store in returnDescription
+        String returnDescription = validateDescription(desc);
         //call the validateDate and store in returnDate
+        String returnDate = validateDate(date);
         //check if the returnDescription is null
+        if(returnDescription != null){
             //set the newItem values
+            newItem.setDescription(returnDescription);
             //set the newItem values
+            newItem.setDueDate(returnDate);
             //it reaches to here set completed to false
+            newItem.setIsCompleted(false);
             //add newItem to list
-            //call createPopUp and return null
+            return newItem;
+        } else{
+            popup.createPopup();
+            return null;
+        }
     }
-
     //validate the description to check if its between 1-256 items
     private String validateDescription(String description){
+        if(description == null || description.length() > 256 || description.isEmpty()){
             //create a variable to pass to another function for pop up description
+            return null;
+        } else{
             //set the description to the newItem
+            return description;
+        }
     }
 
     //validate the datePicker to check if its in gregorian format
     private String validateDate(LocalDate date) {
         //check if matcher.matches
-            //if not null return date.ToString
-            //else return an empty string
+        if(date != null){
+            return date.toString();
+        }else{
+            return "";
+        }
     }
 }
